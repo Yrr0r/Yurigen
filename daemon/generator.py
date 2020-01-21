e9324dca67fffddb1a6da5d23ff7ffe449a6d064
Yrr0r <30972848+Yrr0r@users.noreply.github.com>
2020-01-22 01:16:19 +0800

    generator done

---
+++ daemon/generator.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import os
+import sys
+import configparser
+
+CONFIG_FILE_NAME = 'yurisora.ini'
+
+#chdir to the root and then run command.
+def callgen(path, command):
+    pathbefore = os.getcwd()
+    os.chdir(path)
+    try:
+        result = os.popen(command).read()
+    except:
+        result = "Build Error"
+    
+    os.chdir(pathbefore)
+    return result
+
+def useconf(path):
+    pathbefore = os.getcwd()
+    os.chdir(path)
+    if (os.path.isfile(CONFIG_FILE_NAME) == False):
+        return 'Error: No Config'
+    else:
+        try:
+            config = configparser.ConfigParser()
+            config.read(CONFIG_FILE_NAME)
+            command = config['common']['generator']
+        except:
+            return 'Error: Bad Config'
+        
+        return(callgen(path, command))
+
+    os.chdir(pathbefore)
+
+
+path = sys.argv[1]
+print(useconf(path))
